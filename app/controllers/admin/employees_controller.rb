69284f68e641178c761cf3adfbce71bcf83ed5a0
Yosuke-Masuda <y.m090918@gmail.com>
2024-03-28 17:01:37 +0000

    [Add]adminの社員一覧・詳細

--- app/controllers/admin/employees_controller.rb
+++ app/controllers/admin/employees_controller.rb
@@ -1,10 +1,12 @@
 class Admin::EmployeesController < ApplicationController
   def index
-
+    @company = Company.find(params[:company_id])
+    @employees = @company.employees
   end
 
   def show
-
+    @company = Company.find(params[:company_id])
+    @employee = @company.employees.find(params[:id])
   end
 
 end
