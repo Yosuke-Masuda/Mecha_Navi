db454afc5453e170237b1749f92b59bee9919174
Yosuke-Masuda <y.m090918@gmail.com>
2024-07-22 22:20:25 +0000

    [Fix] admin top

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,7 +2,6 @@ class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
 
   def top
-    @companies = Company.all
     @employees = Employee.page(params[:page]).order(:name)
   end
 
